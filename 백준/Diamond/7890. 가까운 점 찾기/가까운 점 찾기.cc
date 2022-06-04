c3f4fa90c0c8f26e973eaee63787449f59ac472d
HyunBin Lee <68626244+my3ngd@users.noreply.github.com>
2022-06-04 22:43:22 +0900

    [Diamond I] Title: 가까운 점 찾기, Time: 1236 ms, Memory: 19580 KB -BaekjoonHub

---
+++ 백준/Diamond/7890. 가까운 점 찾기/가까운 점 찾기.cc
@@ -0,0 +1,158 @@
+#include <bits/stdc++.h>
+#define ff first
+#define ss second
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pos = pair<ll, ll>;
+const ll INF = LLONG_MAX;
+
+pos operator+(pos p, pos q) { return pos(p.ff+q.ff, p.ss+q.ss); }
+pos operator-(pos p, pos q) { return pos(p.ff-q.ff, p.ss-q.ss); }
+pos operator*(pos p, ll& f) { return pos(p.ff*f, p.ss*f); }
+pos operator/(pos p, ll& f) { return pos(p.ff/f, p.ss/f); }
+ll  operator*(pos p, pos q) { return p.ff*q.ff + p.ss*q.ss; }
+ll  operator/(pos p, pos q) { return p.ff*q.ss - p.ss*q.ff; }
+pos& operator+=(pos& p, pos q) { return p = p + q; }
+pos& operator-=(pos& p, pos q) { return p = p - q; }
+pos& operator*=(pos& p, ll& f) { return p = p * f; }
+pos& operator/=(pos& p, ll& f) { return p = p / f; }
+ll ccw(pos a, pos b, pos c) { return (b-a)/(c-a); }
+int _ccw(pos a, pos b) { ll CCW = a/b; return (CCW > 0) - (CCW < 0); }
+int _ccw(pos a, pos b, pos c) { return _ccw(b-a, c-a); }
+ll  dist(pos p, pos q) { pos r = p-q; return r*r; }
+template <typename T> istream& operator>>(istream& is, pair<T,T>& p) { return is >> p.ff >> p.ss; }
+template <typename T> ostream& operator<<(ostream& os, pair<T,T>& p) { return os << p.ff << " " << p.ss; }
+
+struct node
+{
+    pos p, st, en;
+    bool dir, exist;  // 0: lr, 1: ud
+    node() = default;
+    node(pos p, pos st, pos en, bool dir) : p(p), st(st), en(en), dir(dir) {}
+};
+
+
+struct kd_tree
+{
+    vector<node> tree;
+    vector<pos> arr;
+    ll res;
+
+    kd_tree(vector<pos>& points): arr(points)
+    {
+        int n = 1;
+        while (n < points.size())
+            n <<= 1;
+        tree.resize(n<<1);
+        init(0, arr.size()-1, 1);
+    }
+
+    bool comp_horizontal(pos p, pos q) { return p < q; }
+    bool comp_vertical(pos p, pos q) { return pos(p.ss, p.ff) < pos(q.ss, q.ff); }
+    ll sq(ll x) { return x*x; }
+
+    // range [s, e] (both-closed)
+    void init(int s, int e, int idx)
+    {
+        ll min_x = INF, min_y = INF, max_x = -INF, max_y = -INF;
+        for (int i = s; i <= e; i++)
+        {
+            min_x = min(min_x, arr[i].ff);
+            min_y = min(min_y, arr[i].ss);
+            max_x = max(max_x, arr[i].ff);
+            max_y = max(max_y, arr[i].ss);
+        }
+        tree[idx].dir = (max_x - min_x < max_y - min_y);  // if leftside is smaller -> vertically oriented
+        sort(arr.begin()+s, arr.begin()+e+1, [&](pos a, pos b)
+        {
+            return tree[idx].dir ? comp_vertical(a, b) : comp_horizontal(a, b);
+        });
+        int m = s + e >> 1;
+        tree[idx] = node(arr[m], pos(min_x, min_y), pos(max_x, max_y), tree[idx].dir);
+        tree[idx].exist = true;
+        if (s < m) init(s, m-1, idx<<1|0);
+        if (m < e) init(m+1, e, idx<<1|1);
+        return ;
+    }
+
+    void solve(int idx, pos p)
+    {
+        if (p != tree[idx].p)
+            res = min(res, dist(p, tree[idx].p));
+        if (tree[idx].dir)  // vertical case
+        {
+            if (!comp_vertical(tree[idx].p, p))
+            {
+                if (tree[idx<<1|0].exist)
+                    solve(idx<<1|0, p);
+                if (tree[idx<<1|1].exist && sq(tree[idx<<1|1].st.ss - p.ss) < res)
+                    solve(idx<<1|1, p);
+            }
+            else
+            {
+                if (tree[idx<<1|1].exist)
+                    solve(idx<<1|1, p);
+                if (tree[idx<<1|0].exist && sq(tree[idx<<1|0].en.ss - p.ss) < res)
+                    solve(idx<<1|0, p);
+            }
+        }
+        else
+        {
+            if (!comp_horizontal(tree[idx].p, p))
+            {
+                if (tree[idx<<1|0].exist)
+                    solve(idx<<1|0, p);
+                if (tree[idx<<1|1].exist && sq(tree[idx<<1|1].st.ff - p.ff) < res)
+                    solve(idx<<1|1, p);
+            }
+            else
+            {
+                if (tree[idx<<1|1].exist)
+                    solve(idx<<1|1, p);
+                if (tree[idx<<1|0].exist && sq(tree[idx<<1|0].en.ff - p.ff) < res)
+                    solve(idx<<1|0, p);
+            }
+        }
+    }
+
+    void print_tree(void)
+    {
+        for (int i = 1; i < tree.size(); i <<= 1)
+        {
+            for (int j = i; j < i<<1; j++)
+                if (tree[j].exist)
+                    cout << "[" << tree[j].p << "] ";
+                else
+                    cout << "(" << tree[j].p << ") ";
+            cout << endl;
+        }
+    }
+};
+
+
+
+void run(void)
+{
+    int n; cin >> n;
+    vector<pos> arr(n);
+    for (auto& p: arr) cin >> p;
+    kd_tree kd(arr);
+    for (auto& p: arr)
+    {
+        kd.res = INF;
+        kd.solve(1, p);
+        cout << kd.res << "\n";
+    }
+}
+
+
+int main(int argc, char const *argv[])
+{
+    cin.tie(NULL)->sync_with_stdio(false);
+    int T; cin >> T;
+    while (T--) run();
+    return 0;
+}
+
+
